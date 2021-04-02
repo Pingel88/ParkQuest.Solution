03e8deef23eaaa49404cc6dec9e36fcb61a4389c
Michael Pingel <mdpingel@gmail.com>
2021-04-02 15:04:17 -0700

    add initial database migrations, update Program.cs indenting to be consistent with the rest of the project

---
+++ ParkQuest/Migrations/20210402215815_initial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParkQuest.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Parks",
+                columns: table => new
+                {
+                    ParkId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: false),
+                    ParkType = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    HasRestrooms = table.Column<bool>(type: "tinyint(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Parks", x => x.ParkId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Parks");
+        }
+    }
+}
