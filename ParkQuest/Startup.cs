ddc4830a888725acb7570d5b5d0b0bd71ab6b7ca
Michael Pingel <mdpingel@gmail.com>
2021-04-02 12:11:23 -0700

    modify startup.cs to work with an api rather than mvc

--- ParkQuest/Startup.cs
+++ ParkQuest/Startup.cs
@@ -1,47 +1,35 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using ParkQuest.Models;
-
 namespace ParkQuest
 {
   public class Startup
   {
-    public Startup(IWebHostEnvironment env)
+    public Startup(IConfiguration configuration)
     {
-      var builder = new ConfigurationBuilder()
-        .SetBasePath(env.ContentRootPath)
-        .AddJsonFile("appsettings.json");
-      Configuration = builder.Build();
+      Configuration = configuration;
     }
 
-    public IConfigurationRoot Configuration { get; set; }
+    public IConfiguration Configuration { get; }
 
     public void ConfigureServices(IServiceCollection services)
     {
-      services.AddMvc();
-      services.AddEntityFrameworkMySql()
-        .AddDbContext<ParkQuestContext>(options => options
-        .UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
+      services.AddDbContext<ParkQuestContext>(opt =>
+        opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
+      services.AddControllers();
     }
 
-    public void Configure(IApplicationBuilder app)
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
-      app.UseDeveloperExceptionPage();
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
       app.UseRouting();
-      app.UseStaticFiles();
 
-      app.UseEndpoints(routes => 
-      {
-        routes.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
-      });
+      app.UseAuthorization();
 
-      app.Run(async (context) =>
+      app.UseEndpoints(endpoints =>
       {
-        await context.Response.WriteAsync("Hello World!");
+        endpoints.MapControllers();
       });
     }
   }
