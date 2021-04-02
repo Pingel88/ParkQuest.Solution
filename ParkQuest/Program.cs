a57f5b08f61fc4cc42295242d25a64dcffbc4e52
Michael Pingel <mdpingel@gmail.com>
2021-04-02 14:57:49 -0700

    modify Program.cs to work with an api rather than mvc

--- ParkQuest/Program.cs
+++ ParkQuest/Program.cs
@@ -1,20 +1,20 @@
-using System.IO;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 namespace ParkQuest
 {
-  public class Program
-  {
-    public static void Main(string[] args)
+    public class Program
     {
-      var host = new WebHostBuilder()
-        .UseKestrel()
-        .UseContentRoot(Directory.GetCurrentDirectory())
-        .UseIISIntegration()
-        .UseStartup<Startup>()
-        .Build();
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
 
-      host.Run();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
-  }
 }
