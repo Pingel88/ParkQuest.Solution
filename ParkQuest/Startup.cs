89dfc536a324820dc889db34903f1b9ca61962b2
Michael Pingel <mdpingel@gmail.com>
2021-04-02 14:27:43 -0700

    add more using directives to startup.cs

--- ParkQuest/Startup.cs
+++ ParkQuest/Startup.cs
@@ -2,6 +2,9 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using ParkQuest.Models;
 
 namespace ParkQuest
 {
