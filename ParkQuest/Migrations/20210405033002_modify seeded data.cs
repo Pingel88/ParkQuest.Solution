089da0c514c61ef55614641521fdb246397e6b5a
Michael Pingel <mdpingel@gmail.com>
2021-04-04 20:31:18 -0700

    modify seeded data

---
+++ ParkQuest/Migrations/20210405033002_modify seeded data.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParkQuest.Migrations
+{
+    public partial class modifyseededdata : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 2,
+                column: "HasRestrooms",
+                value: "Unknown");
+
+            migrationBuilder.UpdateData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 7,
+                column: "HasRestrooms",
+                value: "Unknown");
+
+            migrationBuilder.UpdateData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 22,
+                column: "HasRestrooms",
+                value: "Unknown");
+
+            migrationBuilder.UpdateData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 25,
+                column: "HasRestrooms",
+                value: "Unknown");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 2,
+                column: "HasRestrooms",
+                value: "Yes");
+
+            migrationBuilder.UpdateData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 7,
+                column: "HasRestrooms",
+                value: "No");
+
+            migrationBuilder.UpdateData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 22,
+                column: "HasRestrooms",
+                value: "Yes");
+
+            migrationBuilder.UpdateData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 25,
+                column: "HasRestrooms",
+                value: "No");
+        }
+    }
+}
--- ParkQuest/Models/ParkQuestContext.cs
+++ ParkQuest/Models/ParkQuestContext.cs
@@ -16,16 +16,16 @@ namespace ParkQuest.Models
     protected override void OnModelCreating(ModelBuilder builder)
     {
       base.OnModelCreating(builder);
-      
+
       builder.Entity<Park>()
         .HasData(
           new Park { ParkId = 1, Name = "Ainsworth", ParkType = "State", HasRestrooms = "No" },
-          new Park { ParkId = 2, Name = "Crater Lake", ParkType = "National", HasRestrooms = "Yes" },
+          new Park { ParkId = 2, Name = "Crater Lake", ParkType = "National", HasRestrooms = "Unknown" },
           new Park { ParkId = 3, Name = "Gates of the Arctic", ParkType = "National", HasRestrooms = "No" },
           new Park { ParkId = 4, Name = "Bob Straub", ParkType = "State", HasRestrooms = "Yes" },
           new Park { ParkId = 5, Name = "William M. Tugman", ParkType = "State", HasRestrooms = "Yes" },
           new Park { ParkId = 6, Name = "Brian Booth - Beaver Creek", ParkType = "State", HasRestrooms = "Yes" },
-          new Park { ParkId = 7, Name = "Lake Clark", ParkType = "National", HasRestrooms = "No" },
+          new Park { ParkId = 7, Name = "Lake Clark", ParkType = "National", HasRestrooms = "Unknown" },
           new Park { ParkId = 8, Name = "Cascadia", ParkType = "State", HasRestrooms = "No" },
           new Park { ParkId = 9, Name = "Isle Royale", ParkType = "National", HasRestrooms = "Yes" },
           new Park { ParkId = 10, Name = "Kobuk Valley", ParkType = "National", HasRestrooms = "No" },
@@ -40,10 +40,10 @@ namespace ParkQuest.Models
           new Park { ParkId = 19, Name = "Prineville Reservoir", ParkType = "State", HasRestrooms = "No" },
           new Park { ParkId = 20, Name = "Nehalem Bay", ParkType = "State", HasRestrooms = "No" },
           new Park { ParkId = 21, Name = "Congaree", ParkType = "National", HasRestrooms = "Yes" },
-          new Park { ParkId = 22, Name = "Port Orford Heads", ParkType = "State", HasRestrooms = "Yes" },
+          new Park { ParkId = 22, Name = "Port Orford Heads", ParkType = "State", HasRestrooms = "Unknown" },
           new Park { ParkId = 23, Name = "Black Canyon of the Gunnison", ParkType = "National", HasRestrooms = "Yes" },
           new Park { ParkId = 24, Name = "Umpqua Lighthouse", ParkType = "State", HasRestrooms = "Yes" },
-          new Park { ParkId = 25, Name = "Guadalupe Mountains", ParkType = "National", HasRestrooms = "No" }
+          new Park { ParkId = 25, Name = "Guadalupe Mountains", ParkType = "National", HasRestrooms = "Unknown" }
         );
     }
   }
